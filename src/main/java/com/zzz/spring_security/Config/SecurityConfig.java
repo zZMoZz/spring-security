f4472712d69a614ddfab7ea50dd15cafbeb3bcee
zmuhsenz <zmohamedmohsnz@gmail.com>
2024-12-04 20:12:27 +0200

    manipulate with PasswordEncoder

--- src/main/java/com/zzz/spring_security/Config/SecurityConfig.java
+++ src/main/java/com/zzz/spring_security/Config/SecurityConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  1. we put "/error" in permitted requests, to make error accessible by anyone.
@@ -30,16 +37,50 @@ public class SecurityConfig {
     }
 
     /*
-    I add this bean because in "AuthController" class hey say I can't
-    autowired PasswordEncoder as no bean of its type. You may say but
-    there is passwordEncoder where It used in login operation. You alright
-    but the Authentication provider creates an instance of it locally it is
-    not a bean, he create an instance of one of implementations of "PasswordEncode"
-    then assign it to a variable of "PasswordEncode" and use it. we will make same as
-    Authentication provider but make this setup as a bean to can use by other components
+    1. Of course, we use "DelegatingPasswordEncoder" because it is the best one.
+    2. NOTE: don't make multiple beans of the same object as this, because spring will conflict, which one to choose.
+    3. if we run the app, spring will conflict.
+    4. I do that only for explain concepts.
     */
+
+    // case 1 :
+    // this configuration is used by authentication provider by default in login process.
+    // but to can same configuration in spring component, you must create a bean of password encoder.
+    // without it, if you try to inject passwordEncoder to any class, an error occur.
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder1() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    // case 2 :
+    // You want to use "DelegatingPasswordEncoder" with your own configuration
+    @Bean
+    public PasswordEncoder passwordEncoder2() {
+        // define a map to hold encoders and associated prefix.
+        Map<String, PasswordEncoder> encoderMap = new HashMap<>();
+
+        // Add encoders to map
+        encoderMap.put("bcrypt", new BCryptPasswordEncoder());
+        encoderMap.put("scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+        encoderMap.put("noop", NoOpPasswordEncoder.getInstance());
+
+        // Return, with make "bcrypt" as the default
+        return new DelegatingPasswordEncoder("bcrypt", encoderMap);
+    }
+
+    // case 3 :
+    // You want to use default "DelegatingPasswordEncoder" but in same time configure bcrypt.
+    // Note: You can't modify the map, If you want so use above way.
+    @Bean
+    public PasswordEncoder passwordEncoder3() {
+        DelegatingPasswordEncoder delegatingPasswordEncoder = (DelegatingPasswordEncoder)
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2A, 20);
+        delegatingPasswordEncoder.setDefaultPasswordEncoderForMatches(bCryptPasswordEncoder);
+        
+        return  delegatingPasswordEncoder;
+    }
+
 }
+
